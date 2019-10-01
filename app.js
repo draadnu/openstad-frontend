a2936a6f47f9dba53cd9e11159c671d98c00f5e9
Artan Fetahaj <artan@draad.nu>
2019-10-01 08:52:31 +0200

    de brinken changes

--- app.js
+++ app.js
@@ -365,7 +365,7 @@ function run(id, siteData, callback) {
               },
             },
             'title' : {},
-      //      'user-form' : {},
+            'user-form' : {},
             'local-video': {
               addLabel: 'Video (upload)',
             },
