7b45fa0c158dcfe4a5f9f4468125e67101e1ab79
stijnvdvegt <stijnvandervegt@gmail.com>
2019-11-14 20:55:34 +0100

    fix app path

--- ecosystem/app.js
+++ ecosystem/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   name: "openstad-frontend",
-  script: "../app.js",
+  script: "app.js",
   time: true,
   node_args: "--max_old_space_size=4096",
   env: process.env,
