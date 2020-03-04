2080ef911a61efec7114a375364ceb8aa674b124
Rudi van Hierden <rudi@draad.nl>
2020-03-04 08:59:03 +0100

    First version (MVP) of the extra votes system

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -89,7 +89,6 @@ module.exports = {
         'user-form-widgets': {},
         'submissions-widgets': {},
         'participatory-budgeting-widgets': {},
-        //'begroot-widgets': {},
         'local-video-widgets': {},
         'one-row-widgets': {},
         'image-widgets': {},
