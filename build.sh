81c51078ae8c7d3b4469566e522c4f1bf24cc83b
Rudi van Hierden <rudi@draad.nl>
2021-03-26 14:30:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sleep 15
+#sleep 15
 mongo default --eval 'db.createUser({user:"ci",pwd:"test",roles:["readWrite"]});'
-npm install --production
-npm run test -- --coverage
+npm ci --production --prefer-offline --no-audit
+#npm run test -- --coverage
 mkdir assets
 NODE_ENV=production APOS_MINIFY=1 APOS_WORKFLOW=ON APOS_BUNDLE=assets node apostrophe.js apostrophe:generation --create-bundle assets
 docker build -t ${IMAGE_TAG} .
