adceed2cf7bfdcfeca97dbf2cdfae269a9f90eac
Rudi van Hierden <rudi@draad.nl>
2021-04-16 14:07:42 +0200

    Log headers & ip

--- packages/cms/lib/modules/idea-pages/index.js
+++ packages/cms/lib/modules/idea-pages/index.js
@@ -135,6 +135,9 @@ module.exports = {
      const apiUrl = self.apos.settings.getOption(req, 'apiUrl');
      const siteId = req.data.global.siteId;
      const postUrl = `${apiUrl}/api/site/${siteId}/vote`;
+     
+     console.log ('====> VOTE', req.headers, req.connection.remoteAddress);
+     
      const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
      let votes = req.body.votes ? req.body.votes : [{
